976faeccd6dc1fa95d46f7de83d74373d619d303
OHutchyHutch <ohutchins@comcast.net>
2021-08-24 21:11:30 -0400

    Pass 36

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,5 +1,6 @@
 $(function(){
     $("#navbar").load("views/navbar.html"); 
     $("#tos").load("views/tos.html");
+    $("#privacy").load("views/privacy.html")
     $("#footer").load("views/footer.html");
 });
